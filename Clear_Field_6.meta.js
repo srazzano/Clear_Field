19a6cf1657f45877e7631d48a01b421fd33d1747
Sonny Razzano <srazzano@gmail.com>
2017-03-18 11:21:57 -0700

    Add files via upload

---
+++ Clear_Field_6.meta.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        Clear Field 6
+// @version     1.0.7
+// @description Adds clear field button to inputs/textareas
+// @author      Sonny Razzano aka srazzano
+// @namespace   srazzano
+// @include     *
+// @require     https://raw.githubusercontent.com/srazzano/GM_devtools_Dialog/master/GM_devtools_Dialog.js
+// @icon        https://raw.githubusercontent.com/srazzano/Images/master/clear.png
+// @homepageURL https://github.com/srazzano/Clear_Field_6/blob/master/Clear_Field_6.user.js
+// @download    https://raw.githubusercontent.com/srazzano/Clear_Field_6/master/Clear_Field_6.user.js
+// @updateURL   https://raw.githubusercontent.com/srazzano/Clear_Field_6/master/Clear_Field_6.meta.js
+// @support     srazzano@gmail.com
+// @grant       GM_addStyle
+// @grant       GM_getValue
+// @grant       GM_setValue
+// ==/UserScript==
