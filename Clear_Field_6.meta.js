65f3354ab2bec12116b9cf5716f0d673f7047dcd
Sonny Razzano <srazzano@gmail.com>
2017-03-18 06:59:53 -0700

    Update Clear_Field_6.meta.js

--- Clear_Field_6.meta.js
+++ Clear_Field_6.meta.js
@@ -7,8 +7,8 @@
 // @include     *
 // @require     https://raw.githubusercontent.com/srazzano/GM_devtools_Dialog/master/GM_devtools_Dialog.js
 // @icon        https://raw.githubusercontent.com/srazzano/Images/master/clear.png
-// @downloadURL https://raw.githubusercontent.com/srazzano/Clear_Field/master/Clear_Field_6.user.js
-// @updateURL   https://raw.githubusercontent.com/srazzano/Clear_Field/master/Clear_Field_6.meta.js
+// @downloadURL https://raw.githubusercontent.com/srazzano/Clear_Field_6/master/Clear_Field_6.user.js
+// @updateURL   https://raw.githubusercontent.com/srazzano/Clear_Field_6/master/Clear_Field_6.meta.js
 // @support     srazzano@gmail.com
 // @grant       GM_addStyle
 // @grant       GM_getValue
